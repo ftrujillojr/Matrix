6c58ea65aa6f92eec3f4da0727dbeb364f71bcb3
Francis Trujillo <ftrujillojr@gmail.com>
2014-11-22 18:42:21 -0700

    push

--- src/org/trujillo/francis/matrix/MatrixMain.java
+++ src/org/trujillo/francis/matrix/MatrixMain.java
@@ -7,43 +7,43 @@ public class MatrixMain {
 
     public static void main(String[] args) {
         try {
-            double[][] dArray = new double[][]{
+            double[][] mat = new double[][]{
                 {3, 2, -1},
                 {2, -2, 4},
-                {-1, 1.0 / 2.0, -1},};
+                {-1, 1.0 / 2.0, -1}
+            };
 
-            double[][] dArray2 = new double[][]{
+            double[][] vec = new double[][]{
                 {1},
                 {-2},
-                {0},};
-            
-            //Matrix.setShowWork();
-            //Matrix.setScienticOut();
+                {0}
+            };
 
-            Matrix matrix = new Matrix(dArray);
-            Matrix vector = new Matrix(dArray2);
-            
+            Matrix matrix = new Matrix(mat);
+            Matrix vector = new Matrix(vec);
+
             System.out.println("Matrix \n" + matrix.toString());
-            System.out.println("====================================");
-            
+
             System.out.println("Vector \n" + vector.toString());
-            System.out.println("====================================");
-            
+
             Matrix invMatrix = Matrix.inverse(matrix);
             System.out.println("Inverse Matrix \n" + invMatrix.toString());
-            System.out.println("====================================\n");
+
+            boolean verifyInvMat = false;
+            if (verifyInvMat) {
+                Matrix.setShowWork();
+                System.out.println("(Verify INV Matrix is correct) Multiplying Original Matrix with the Inverse Matrix == IDENTITY Matrix\n");
+                Matrix resultMatrix = Matrix.multiply(matrix, invMatrix);
+                Matrix.clrShowWork();
+            }
 
             Matrix.setShowWork();
-            System.out.println("(Verify INV Matrix is correct) Multiplying Original Matrix with the Inverse Matrix == IDENTITY Matrix\n");
-            Matrix resultMatrix = Matrix.multiply(matrix, invMatrix);
-            System.out.println("RESULT from multiply \n" + resultMatrix.toString());
-            System.out.println("====================================\n");
-
-            Matrix sysOfEq = Matrix.multiply(invMatrix, vector);
+            System.out.println("Multiply  Inverse Matrix by Vector to solve linear system of equations.");
+            Matrix resultSysOfEq = Matrix.multiply(invMatrix, vector);
             Matrix.clrShowWork();
-            System.out.println("Solved System of equations \n" + sysOfEq.toString());
-            System.out.println("====================================\n");
-            
+            System.out.println(" \n" + resultSysOfEq.toString());
+            System.out.println("===============================================================\n");
+
         } catch (NoSquareException ex) {
             Logger.getLogger(MatrixMain.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IllegalDimensionException ex) {
